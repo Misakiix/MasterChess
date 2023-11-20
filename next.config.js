38afd5a8a4ecd4a9ee8fa92aa71962f67035e58e
Misakiix <misakihayatop@gmail.com>
2023-11-20 16:01:33 -0300

    Teste 6

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const nextConfig = {
     env: {
-        NEXT_PUBLIC_URL: 'http://localhost:3000',
+        NEXT_PUBLIC_URL: 'https://masterchessbeta.vercel.app',
         JWT_SECRET: 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY5ODc4MjkzNywiaWF0IjoxNjk4NzgyOTM3fQ.EmGqQxpjjnWt_qDwH0q0-emzceU4JEzb-BKdoHYiaPc',
         DATABASE_URL: 'postgres://postgres.eulyxwlkddhmmnfxlypb:J10j10j10419!@aws-0-sa-east-1.pooler.supabase.com:6543/postgres'
     },
